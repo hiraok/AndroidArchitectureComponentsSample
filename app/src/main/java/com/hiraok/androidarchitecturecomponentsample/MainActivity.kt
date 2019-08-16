8cc1f22754f59ef01004fcb6a3ecc0fc36094240
hiraok <khirao.dev.work@gmail.com>
2019-08-16 11:53:42 +0900

    add resource

--- app/src/main/java/com/hiraok/androidarchitecturecomponentsample/MainActivity.kt
+++ app/src/main/java/com/hiraok/androidarchitecturecomponentsample/MainActivity.kt
@@ -2,11 +2,20 @@ package com.hiraok.androidarchitecturecomponentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
+import kotlinx.android.synthetic.main.toolbar.toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpToolbar()
     }
+
+    private fun setUpToolbar() {
+       toolbar.title = getString(R.string.string_main)
+    }
+
 }
